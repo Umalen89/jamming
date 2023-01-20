1fecc7df8a24a29f33b1d0d73c49448a1a8a4966
Umalen89 <mdelgadoobrero@gmail.com>
2023-01-20 11:44:33 +0000

    Starting react App

---
+++ src/Components/App/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Ja<span class="highlight">mmm</span>ing</h1>
+      <div class="App">
+        {/*<!-- Add a SearchBar component -->8*/}
+        <div class="App-playlist">
+          {/*<!-- Add a SearchResults component -->*/}
+          {/*<!-- Add a Playlist component -->*/}
+        </div>
+      </div>
+    </div>
+      );
+  }
+
+}
+
+export default App;
