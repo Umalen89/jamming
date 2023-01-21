4745b704ac3ee2dc4d61c2160adf7c3ed6ee73bf
Umalen89 <mdelgadoobrero@gmail.com>
2023-01-21 00:49:13 +0000

    Finish Components to render App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,19 @@
 //import logo from './logo.svg';
 
-import SearchResults from '../SearchResults/SearchResults'
-import Playlist from '../Playlist/Playlist ';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 import  SearchBar  from '../SearchBar/SearchBar';
 import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { searchResults:[{id: 1, name:'Wicked Game', artist:'G-Eazy', album:'This things happends too'},
+                                  {id:2, name:'Black Spiderman', artist:'Logic', album:'Everybody'},
+                                  {id:3, name:'Enemy',artist:'Imagine Dragons', album:'League of Legends ARCANE theme song'}]}
+  }
+
   render() {
     return (
       <div>
@@ -16,7 +23,7 @@ class App extends React.Component {
         <SearchBar />
         <div className="App-playlist">
           {/*<!-- Add a SearchResults component -->*/}
-          <SearchResults />
+          <SearchResults searchResults={this.state.searchResults}/>
           {/*<!-- Add a Playlist component -->*/}
           <Playlist />
         </div>
